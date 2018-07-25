2ca05a54b5868686342e826b7b0aac11c658ccaa
chevyng <zhi.ng.14@ucl.ac.uk>
2018-07-25 11:31:02 +0100

    updated utils file
    In utils file, compute_ap function now returns mAP and list of AP from different iou_threshold_level[0.5:95].
    
    This allows us to compute mean AP at each iou_threshold_level instead of mean AP over the whole range

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -786,11 +786,11 @@ def compute_ap_range(gt_box, gt_class_id, gt_mask,
         if verbose:
             print("AP @{:.2f}:\t {:.3f}".format(iou_threshold, ap))
         AP.append(ap)
-    AP = np.array(AP).mean()
+    mAP = np.array(AP).mean()
     if verbose:
         print("AP @{:.2f}-{:.2f}:\t {:.3f}".format(
-            iou_thresholds[0], iou_thresholds[-1], AP))
-    return AP
+            iou_thresholds[0], iou_thresholds[-1], mAP))
+    return mAP , AP
 
 
 def compute_recall(pred_boxes, gt_boxes, iou):
